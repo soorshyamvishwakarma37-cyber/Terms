2bff675a4b8ad45223484886fe0cca40288ebe71
soorshyamvishwakarma37-cyber <255987687+soorshyamvishwakarma37-cyber@users.noreply.github.com>
2026-09-06 18:52:39 +0530

    Delete 02-init.js

--- 02-init.js
+++
@@ -1,8 +0,0 @@
-/* SewaAstra Customer — 02-init.js */
-/* Firebase config / शुरुआती setup. */
-
-/* ═══ inline ब्लॉक 1/1 ═══ */
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-52QKXTC1J4');
